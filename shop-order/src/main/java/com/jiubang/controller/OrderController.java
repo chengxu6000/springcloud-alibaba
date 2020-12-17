39a6ebedf3bf9bd7eafd2a1c11a0d112d633672a
chengxu6000 <627112256@qq.com>
2020-12-17 22:35:36 +0800

    继续拓展

--- shop-order/src/main/java/com/jiubang/controller/OrderController.java
+++ shop-order/src/main/java/com/jiubang/controller/OrderController.java
@@ -4,12 +4,18 @@ import com.alibaba.fastjson.JSON;
 import com.jiubang.domain.Order;
 import com.jiubang.domain.Product;
 import com.jiubang.service.OrderService;
+import com.jiubang.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Random;
 
 @RestController
 @Slf4j
@@ -18,13 +24,24 @@ public class OrderController {
     private RestTemplate restTemplate;
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+    @Autowired
+    private ProductService productService;
     //准备买1件商品
     @GetMapping("/order/prod/{pid}")
     public Order order(@PathVariable("pid") Integer pid) {
         log.info(">>客户下单，这时候要调用商品微服务查询商品信息");
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("service-product");
+        //自定义负载均衡
+//        int index = new Random().nextInt(instances.size());
+//
+//        ServiceInstance serviceInstance = instances.get(index);
+//        String url = "service-product";
+//        log.debug(url);
         //通过restTemplate调用商品微服务
-        Product product = restTemplate.getForObject(
-                        "http://localhost:8081/product/" + pid, Product.class);
+        Product product = productService.findByPid(pid);
 
         log.info(">>商品信息,查询结果:" + JSON.toJSONString(product));
         Order order = new Order();
---
+++ shop-order/src/main/java/com/jiubang/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.jiubang.service;
+
+import com.jiubang.domain.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "service-product")
+public interface ProductService {
+    //指定调用提供者的哪个方法
+    //@FeignClient+@GetMapping 就是一个完整的请求路径 http://serviceproduct/product/{pid}
+    @GetMapping(value = "/product/{pid}")
+    Product findByPid(@PathVariable("pid") Integer pid);
+
+}
